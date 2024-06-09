d1198b59d6d180f06a74839962026d58020345c5
Sanjay Sajnani <138214709+TELIBO@users.noreply.github.com>
2024-06-09 17:23:14 +0000

    js

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,2 +1,10 @@
 console.log(typeof null);
 console.log(typeof undefined);
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(2+2+"1");
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined<0)
+console.log(undefined<=0)
