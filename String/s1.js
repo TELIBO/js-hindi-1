5375357be57cfebabc1af012e865380418c0421c
Sanjay Sajnani <138214709+TELIBO@users.noreply.github.com>
2024-06-11 10:50:11 +0000

    s1

---
+++ String/s1.js
@@ -0,0 +1,18 @@
+const mysym=Symbol("key1")
+const jsuser={
+    name:"sanjay",
+    "fullname":"sajnani",
+    [mysym]:"mykey1",
+    age:20,
+    location:"aburoad",
+    email:"hitesh@google.com",
+    isLoggedIn:false,
+    lastlogindays:["monday","tuesday"]
+}
+console.log(jsuser[mysym])
+console.log(jsuser["fullname"])
+jsuser.greeting=function(){
+    console.log(`hello user,${this.fullname}`)
+}
+console.log(jsuser.greeting())
+
