be4c0fd379795341b862d1c9716e1d980934b92e
Sanjay Sajnani <138214709+TELIBO@users.noreply.github.com>
2024-06-11 10:18:27 +0000

    array

---
+++ arrays/a1.js
@@ -0,0 +1,10 @@
+const myarr=[0,1,2,3,4,5]
+console.log(myarr[0]);
+const slice=myarr.slice(0,3)
+console.log(slice,myarr)
+const splice=myarr.splice(0,3)
+console.log(splice,myarr)
+const marvel=["sm","im","ca"];
+const dc=["sum","bm","am"]
+console.log(marvel.concat(dc))
+console.log(marvel.sort())
