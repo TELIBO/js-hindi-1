e24185723e8418f09474a63fbd095c16edc4cb18
Sanjay Sajnani <138214709+TELIBO@users.noreply.github.com>
2024-06-22 14:27:25 +0000

    s

--- closures/c1.js
+++ closures/c1.js
@@ -1,3 +1,4 @@
+//closure are used for state management like t declare aglobal variable inside a function so that it is safe ,in simple words inner function can acces the variables of outer function
 function counter(){
     let counter=0;
     function cplus(){
@@ -10,7 +11,7 @@ const count=counter();
 count.cplus();
 count.cplus();
 console.log(count);
-//so to use inner function as property you should make outer function return an object
+//so to use inner function you should make outer function return an object and then use inner function as a property
 function game(){
     let point=0;
     function increasepoints(){
